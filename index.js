8fb4c77f61e6c21acd0c1f1517005e983506f56a
fearless-snippet-5362 <cpk1115@gmail.com>
2019-02-08 18:26:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var height = "74"
 if (name === "Susan") {
   console.log("the name is correct")
 } else {
-  console.log("the name is correct")
+  console.log("expected the name to be Susan")
 }
 
 // Don't worry about this
